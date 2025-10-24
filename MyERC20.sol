f4f5e5f3fc3c52a1343cfee5010363b64ad50330
luisannacocco <98464994+luisannacocco@users.noreply.github.com>
2025-10-24 11:52:41 +0200

    Rename MyERC20 contract to MyToken

--- MyERC20.sol
+++ MyERC20.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyERC20 is ERC20, Ownable {
+contract MyToken is ERC20, Ownable {
 
   address marketplaceContract;
   address marketplaceResponsible;
